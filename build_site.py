cdd79ed2c3a6f9c2947f8b784c2a1471edbfd485
Tyler <chentylerl@gmail.com>
2026-02-22 22:46:17 -0500

    add ICERM

--- build_site.py
+++ build_site.py
@@ -31,16 +31,17 @@ def bib_to_html(pub_idx,bib_entry):
     html += (f'<div class="journal">{bib_entry["journal"].replace("{","").replace("}","")}.</div>\n' if "journal" in bib_entry.keys() else '')\
            +(f'<div class="journal">{bib_entry["booktitle"]}.</div>\n' if "booktitle" in bib_entry.keys() else '')\
            +(f'<div class="year">{bib_entry["year"]}</div>.\n' if "year" in bib_entry.keys() else '')\
-           +(f'<div class="notes">{bib_entry["note"]}</div>\n' if "note" in bib_entry.keys() else '')
+           +(f'<div class="notes">{bib_entry["note"]}.</div>\n' if "note" in bib_entry.keys() else '')
     
     #html+='<br>'
     if "doi" in bib_entry.keys():
         html += f'<div class="doi"><a href="https://doi.org/{bib_entry["doi"]}">[journal]</a></div>\n'
-    elif "url" in bib_entry.keys():
-        html += f'<div class="url"><a href="{bib_entry["url"]}">[journal]</a></div>\n'
     elif "pdf" in bib_entry.keys():
         html += f'<div class="pdf"><a href="{bib_entry["pdf"]}">[pdf]</a></div>\n'
-    elif "video" in bib_entry.keys():
+    elif "url" in bib_entry.keys():
+        html += f'<div class="url"><a href="{bib_entry["url"]}">[journal]</a></div>\n'
+
+    if "video" in bib_entry.keys():
         html += f'<div class="video"><a href="{bib_entry["video"]}">[video]</a></div>\n'
 
     html += (f'<div class="eprint"><a href="https://arxiv.org/abs/{bib_entry["eprint"]}">[arXiv]</a></div>\n' if "eprint" in bib_entry.keys() else '')
@@ -94,7 +95,7 @@ def add_publications(file):
 
     html_new = html_raw
 
-    for regex in [r'\[pub:(.*?)\]',r'\[ipub:(.*?)\]',r'\[talk:(.*?)\]']:
+    for regex in [r'\[mono:(.*?)\]',r'\[pub:(.*?)\]',r'\[ipub:(.*?)\]',r'\[talk:(.*?)\]']:
         pubs = re.findall(regex,html_raw)
         for k,pub in enumerate(pubs):            
             for bib_entry in bib_database:
@@ -102,6 +103,7 @@ def add_publications(file):
             
                     pub_html = bib_to_html(len(pubs)-k,bib_entry)
 
+                    html_new = html_new.replace(f'[mono:{pub}]',pub_html)
                     html_new = html_new.replace(f'[pub:{pub}]',pub_html)
                     html_new = html_new.replace(f'[ipub:{pub}]',pub_html)
                     html_new = html_new.replace(f'[talk:{pub}]',pub_html)
@@ -110,9 +112,9 @@ def add_publications(file):
     f.write(html_new)
     f.close()
 
-build_html('index')
+#build_html('applications/index')
+#build_html('intros/index')
 build_html('talks/index')
-build_html('students/index')
-add_publications('index')
 add_publications('talks/index')
+#build_html('index')
 
